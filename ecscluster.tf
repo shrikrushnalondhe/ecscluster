76f0e6b8de641d906a344e5897e53dd7dc1d1eca
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-10 17:51:19 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -2,19 +2,19 @@ module "ecs_cluster" {
   source = "infrablocks/ecs-cluster/aws"
   version = "3.4.0"
 
-  region = "eu-west-2"
-  vpc_id = "vpc-fb7dc365"
-  subnet_ids = "subnet-eb32c271,subnet-64872d1f"
+  region = "us-east-1"
+  vpc_id = "vpc-0413727213c90fd60"
+  subnet_ids = "subnet-0e4d9be10adee579b,subnet-0583f78d928f94463"
 
   component = "important-component"
   deployment_identifier = "production"
 
   cluster_name = "services"
-  cluster_instance_ssh_public_key_path = "~/.ssh/id_rsa.pub"
-  cluster_instance_type = "t2.small"
+  cluster_instance_ssh_public_key_path = "/home/jenkins/keys/aws_key.pub"
+  cluster_instance_type = "t2.micro"
 
   cluster_minimum_size = 2
-  cluster_maximum_size = 10
+  cluster_maximum_size = 4
   cluster_desired_capacity = 4
 }
 
@@ -22,16 +22,16 @@ module "ecs_load_balancer" {
   source = "infrablocks/ecs-load-balancer/aws"
   version = "0.1.10"
 
-  region = "eu-west-2"
-  vpc_id = "vpc-fb7dc365"
-  subnet_ids = "subnet-ae4533c4,subnet-443e6b12"
+  region = "us-east-1"
+  vpc_id = "vpc-0413727213c90fd60"
+  subnet_ids = "subnet-0e4d9be10adee579b,subnet-0583f78d928f94463"
 
   component = "important-component"
   deployment_identifier = "production"
 
   service_name = "memcache"
   service_port = "11211"
-  service_certificate_arn = "arn:aws:acm:eu-west-2:121408295202:certificate/4e0452c7-d32d-4abd-b5f2-69490e83c936"
+  service_certificate_arn = "arn:aws:acm:us-east-1:885270470374:certificate/d2b7948f-f15f-4a38-b17e-ab6247572580"
 
   domain_name = "example.com"
   public_zone_id = "Z1WA3EVJBXSQ2V"
