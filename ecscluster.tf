a64242e70a3b7be35d75cdd49968680832f67a60
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-13 12:30:47 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -1,19 +1,114 @@
-module "ecs_cluster" {
-  source = "infrablocks/ecs-cluster/aws"
-  version = "3.4.0"
 
-  region = "us-east-1"
-  vpc_id = "vpc-2db7c450"
-  subnet_ids = "subnet-b89563f4,subnet-a595b5fa"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1" # Setting my region to London. Use your own region here
+}
 
-  component = "important-component"
-  deployment_identifier = "production"
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "my-first-ecr-repo" # Naming my repository
+}
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster" # Naming the cluster
+}
 
-  cluster_name = "services"
-  cluster_instance_ssh_public_key_path = "/home/jenkins/keys/aws_key.pub"
-  cluster_instance_type = "t2.micro"
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.my_first_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 1024,
+      "cpu": 1024
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["EC2"] # Stating that we are using ECS EC2
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 1024        # Specifying the memory our container requires
+  cpu                      = 1024         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
 
-  cluster_minimum_size = 2
-  cluster_maximum_size = 4
-  cluster_desired_capacity = 4
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1b"
+}
+
+
+  resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "EC2"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+   # assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
 }
