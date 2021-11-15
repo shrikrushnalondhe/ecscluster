2ca0d8cd1667bf58c88f6c205b4903d1b062084d
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-15 14:53:00 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -77,14 +77,23 @@ tags = {
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "eu-west-2a"
+  tags = {
+  Name = "default_subnet_a"
+}
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
   availability_zone = "eu-west-2b"
+  tags = {
+  Name = "default_subnet_b"
+}
 }
 
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "eu-west-2c"
+  tags = {
+  Name = "default_subnet_c"
+}
 }
 
   network_configuration {
