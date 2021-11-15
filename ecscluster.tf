1943e1ab69559c71c2277b738ccc5d82b6a2d094
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-15 14:26:31 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_ecr_repository" "my_first_ecr_repo" {
   name = "my-first-ecr-repo" # Naming my repository
 }
+
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" # Naming the cluster
 }
@@ -24,15 +25,15 @@ resource "aws_ecs_task_definition" "my_first_task" {
           "hostPort": 3000
         }
       ],
-      "memory": 1024,
-      "cpu": 1024
+      "memory": 512,
+      "cpu": 256
     }
   ]
   DEFINITION
-  requires_compatibilities = ["EC2"] # Stating that we are using ECS EC2
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 1024        # Specifying the memory our container requires
-  cpu                      = 1024         # Specifying the CPU our container requires
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
@@ -58,73 +59,60 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 }
 
 
-# Providing a reference to our default VPC
-#resource "aws_default_vpc" "default_vpc" {
-#}
-
-# Providing a reference to our default subnets
-#resource "aws_default_subnet" "default_subnet_a" {
-#  availability_zone = "us-east-1a"
-#}
-
-#resource "aws_default_subnet" "default_subnet_b" {
-#  availability_zone = "us-east-1b"
-#}
-
-
-  resource "aws_ecs_service" "my_first_service" {
+resource "aws_ecs_service" "my_first_service" {
   name            = "my-first-service"                             # Naming our first service
   cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
   task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
-
-
-
-
-resource "aws_subnet" "subnet_dev" {
-  vpc_id     = "vpc-02f9e5856f2704413"
-  cidr_block = "10.0.4.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "subnet_dev"
-  }
 }
 
-resource "aws_subnet" "subnet_prod" {
-  vpc_id     = "vpc-02f9e5856f2704413"
-  cidr_block = "10.0.5.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
 
-  tags = {
-    Name = "subnet_prod"
-  }
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-2c"
 }
 
 
 
-load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
-    container_port   = 3000 # Specifying the container port
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
   }
-
-   network_configuration {
-    subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
-   # assign_public_ip = true # Providing our containers with public IPs
-  #}
 }
 
-resource "aws_security_group" "service_security_group" {
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
   }
 
   egress {
@@ -132,13 +120,5 @@ resource "aws_security_group" "service_security_group" {
     to_port     = 0 # Allowing any outgoing port
     protocol    = "-1" # Allowing any outgoing protocol
     cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
-
-terraform {
-backend "s3" {
-   bucket = "demobucketecs"
-   key    = "terraform.tfstate"
-   region = "us-east-1"
   }
 }
