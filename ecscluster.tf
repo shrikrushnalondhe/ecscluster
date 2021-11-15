2a410c99c2926bdb75d2ab96685a17fb81ad5beb
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-15 17:17:05 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -96,10 +96,10 @@ resource "aws_subnet" "subnet_prod" {
 output "aws_subnet_subnet_prod" {
   value = "${aws_subnet.subnet_prod.id}"
 }
-#  network_configuration {
- #   subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
-  #  assign_public_ip = true # Providing our containers with public IPs
-  #}
-#}
+  network_configuration {
+    subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
 
 
