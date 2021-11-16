42ac18e389e50883549417eb68beebb46fd5d28b
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-16 12:02:58 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -59,25 +59,6 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 }
 
 
-resource "aws_ecs_service" "my_first_service" {
-  name            = "my-first-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
-  
-    load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
-    container_port   = 3000 # Specifying the container port
-  }
-  
-network_configuration  {
-    subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
-    assign_public_ip = true # Providing our containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
-  }
-}
 
 resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
@@ -140,6 +121,26 @@ resource "aws_security_group" "load_balancer_security_group" {
   }
 }
 
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+  
+    load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+  
+network_configuration  {
+    subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+}
 
 
 resource "aws_lb_target_group" "target_group" {
