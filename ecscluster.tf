32bcfced48f47668386a9d11704afea4e3394a4a
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-16 12:46:40 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-east-1" # Setting my region to London. Use your own region here
 }
 
-resource "aws_ecr_repository" "my_first_ecr_repo1" {
-  name = "my-first-ecr-repo1" # Naming my repository
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "my-first-ecr-repo" # Naming my repository
 }
 
 resource "aws_ecs_cluster" "my_cluster" {
