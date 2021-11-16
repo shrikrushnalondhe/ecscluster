0fed526e50a738bcaa0e131dfa2ed7e67c7b7b1c
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-16 12:54:55 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-east-1" # Setting my region to London. Use your own region here
 }
 
-resource "aws_ecr_repository" "my_first_ecr_repo" {
-  name = "my-first-ecr-repo" # Naming my repository
+resource "aws_ecr_repository" "demoimage1" {
+  name = "demoimage1" # Naming my repository
 }
 
 resource "aws_ecs_cluster" "my_cluster" {
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "my_first_task" {
   [
     {
       "name": "my-first-task",
-      "image": "${aws_ecr_repository.my_first_ecr_repo.repository_url}",
+      "image": "${aws_ecr_repository.demoimage1.repository_url}",
       "essential": true,
       "portMappings": [
         {
