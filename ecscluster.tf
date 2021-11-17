4aa21fce4bb760328a04ddbdac6a85d99f88c948
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-17 08:50:14 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -149,6 +149,23 @@ network_configuration  {
   }
 }
 
+resource "aws_security_group" "service_security_group" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
 
 resource "aws_lb_target_group" "target_group" {
   name        = "target-group"
@@ -181,19 +198,4 @@ resource "aws_lb_listener" "listener" {
 }
 
 
-resource "aws_security_group" "service_security_group" {
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-  }
 
-  egress {
-    from_port   = 0 # Allowing any incoming port
-    to_port     = 0 # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
