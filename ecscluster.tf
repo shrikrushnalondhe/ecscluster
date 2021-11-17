b2b9a52a3565089f3bad28fa706ff761a2465f3f
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-17 10:27:52 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -175,8 +175,8 @@ resource "aws_security_group" "service_security_group" {
 
 resource "aws_lb_target_group" "target_group" {
   name        = "target-group"
-  port        = 443
-  protocol    = "HTTPS"
+  port        = 80
+  protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = "${aws_vpc.vpc.id}" # Referencing the aws VPC
 
@@ -192,8 +192,8 @@ health_check {
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
-  port              = "443"
-  protocol          = "HTTPS"
+  port              = "80"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
