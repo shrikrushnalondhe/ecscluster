c9ffdf6dc463f23d4f0b1302aac7b9e326e7a77a
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-11-17 08:46:07 +0530

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -145,7 +145,7 @@ resource "aws_ecs_service" "my_first_service" {
 network_configuration  {
     subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
     assign_public_ip = true # Providing our containers with public IPs
-  # security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+   security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
   }
 }
 
@@ -177,27 +177,6 @@ resource "aws_lb_listener" "listener" {
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
-  }
-}
-
-
-resource "aws_ecs_service" "my_first_service" {
-  name            = "my-first-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers to 3
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
-    container_port   = 80 # Specifying the container port
-  }
-
-  network_configuration {
-    subnets          = ["${aws_subnet.subnet_dev.id}", "${aws_subnet.subnet_prod.id}"]
-    assign_public_ip = true                                                # Providing our containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
   }
 }
 
